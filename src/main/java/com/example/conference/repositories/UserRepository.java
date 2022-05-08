86a89c8bb5eeb469cc250f787fc7c29f64616cff
Miroslaw Karwowski <mirek5207@gmail.com>
2022-05-08 11:54:55 +0200

    feat: init add repositories for basic operations with database

---
+++ src/main/java/com/example/conference/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.example.conference.repositories;
+
+import com.example.conference.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+}
