8ddb0296d121ea4f8750601983f3fb8955b6b56d
Miroslaw Karwowski <mirek5207@gmail.com>
2022-05-09 13:59:15 +0200

    feat: add native query to get all reserved lecture by user

--- src/main/java/com/example/conference/repositories/LectureRepository.java
+++ src/main/java/com/example/conference/repositories/LectureRepository.java
@@ -1,7 +1,18 @@
 package com.example.conference.repositories;
 
 import com.example.conference.entities.Lecture;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface LectureRepository extends CrudRepository<Lecture,Long> {
+    @Query(value = "SELECT LECTURE.LECTURE_ID,LECTURE.NAME,LECTURE.TIME_FRAME,LECTURE.COURSE_ID FROM LECTURE\n" +
+            "INNER JOIN USER_LECTURE ON USER_LECTURE.LECTURE_ID = LECTURE.LECTURE_ID\n" +
+            "INNER JOIN USER ON USER.USER_ID = USER_LECTURE.USER_ID\n" +
+            "WHERE USER.LOGIN = :login"
+            ,nativeQuery = true)
+    Set<Lecture> getLecture(String login);
+
+
 }
