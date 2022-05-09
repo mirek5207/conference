4630cdf1ddeea0b73b9e2eea3ca09f5e15438fbe
Miroslaw Karwowski <mirek5207@gmail.com>
2022-05-09 14:09:20 +0200

    feat: add get request with service to display all lecture reserved by user

--- src/main/java/com/example/conference/services/UserService.java
+++ src/main/java/com/example/conference/services/UserService.java
@@ -1,9 +1,14 @@
 package com.example.conference.services;
 
+import com.example.conference.entities.Lecture;
 import com.example.conference.entities.User;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
 public interface UserService {
+
     User addUserToLecture(Long lectureId, String login, String email);
+    Set<Lecture> getAllUserLecture(String login);
 }
