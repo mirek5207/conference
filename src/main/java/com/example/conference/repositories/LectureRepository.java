90f4006d7b7c53bc2840884eafd2ca7cdf7c231c
Miroslaw Karwowski <mirek5207@gmail.com>
2022-05-09 18:32:52 +0200

    feat: add native query to delete reservation for lecture

--- src/main/java/com/example/conference/repositories/LectureRepository.java
+++ src/main/java/com/example/conference/repositories/LectureRepository.java
@@ -1,12 +1,15 @@
 package com.example.conference.repositories;
 
 import com.example.conference.entities.Lecture;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
 import java.util.Set;
 
 public interface LectureRepository extends CrudRepository<Lecture,Long> {
+
     @Query(value = "SELECT LECTURE.LECTURE_ID,LECTURE.NAME,LECTURE.TIME_FRAME,LECTURE.COURSE_ID FROM LECTURE\n" +
             "INNER JOIN USER_LECTURE ON USER_LECTURE.LECTURE_ID = LECTURE.LECTURE_ID\n" +
             "INNER JOIN USER ON USER.USER_ID = USER_LECTURE.USER_ID\n" +
@@ -14,5 +17,9 @@ public interface LectureRepository extends CrudRepository<Lecture,Long> {
             ,nativeQuery = true)
     Set<Lecture> getLecture(String login);
 
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE FROM USER_LECTURE WHERE USER_LECTURE.USER_ID = :userId AND USER_LECTURE.LECTURE_ID = :lectureId",nativeQuery = true)
+    void deleteReservedLecture(Long userId, Long lectureId);
 
 }
