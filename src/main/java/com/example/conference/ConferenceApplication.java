3378e5b38958b089df897e286249d6698acaa836
Miroslaw Karwowski <mirek5207@gmail.com>
2022-05-10 20:00:28 +0200

    feat: add run swagger open api on start

--- src/main/java/com/example/conference/ConferenceApplication.java
+++ src/main/java/com/example/conference/ConferenceApplication.java
@@ -3,11 +3,18 @@ package com.example.conference;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.io.IOException;
+
 @SpringBootApplication
 public class ConferenceApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ConferenceApplication.class, args);
+		runSwaggerOpenApiOnStart();
+	}
+	private static void runSwaggerOpenApiOnStart() throws IOException {
+		Runtime rt = Runtime.getRuntime();
+		rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/swagger-ui/index.html");
 	}
 
 }
