7321b49cd819491f6d69b0eed8b29a2274c5d7fa
Miroslaw Karwowski <mirek5207@gmail.com>
2022-05-10 18:54:34 +0200

    feat: add query get percentage of users participating in each course

--- src/main/java/com/example/conference/repositories/CourseRepository.java
+++ src/main/java/com/example/conference/repositories/CourseRepository.java
@@ -1,7 +1,17 @@
 package com.example.conference.repositories;
 
 import com.example.conference.entities.Course;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Set;
+
 public interface CourseRepository extends CrudRepository<Course,Long> {
+
+    @Query(value = "SELECT COURSE.NAME,CONCAT(COUNT(USER_LECTURE.LECTURE_ID) * 100 / 15,  '% obecności w kursie') FROM COURSE\n" +
+            "INNER JOIN LECTURE ON LECTURE.COURSE_ID = COURSE.ID \n" +
+            "INNER JOIN USER_LECTURE ON USER_LECTURE.LECTURE_ID  = LECTURE.LECTURE_ID \n" +
+            "GROUP BY COURSE.ID"
+            ,nativeQuery = true)
+    Set<String> getPercentageOfUsersParticipatingInEachCourse();
 }
