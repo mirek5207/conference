cc9742e42e4eae5c9a1cd3d8db6317276acfb809
Miroslaw Karwowski <mirek5207@gmail.com>
2022-05-11 19:26:59 +0200

    remove: open swagger on start,function worked only on Windows

--- src/main/java/com/example/conference/ConferenceApplication.java
+++ src/main/java/com/example/conference/ConferenceApplication.java
@@ -10,11 +10,5 @@ public class ConferenceApplication {
 
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(ConferenceApplication.class, args);
-		runSwaggerOpenApiOnStart();
 	}
-	private static void runSwaggerOpenApiOnStart() throws IOException {
-		Runtime rt = Runtime.getRuntime();
-		rt.exec("rundll32 url.dll,FileProtocolHandler " + "http://localhost:8080/swagger-ui/index.html");
-	}
-
 }
